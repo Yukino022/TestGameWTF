3f3416ab880b29b2c874dbd9cafd064d6b0d89b3
qu1kk <gomofobi34@gmail.com>
2026-05-30 22:53:34 +0500

    123

--- player.gd
+++ player.gd
@@ -11,6 +11,8 @@ const AIR_ATTACK_DURATION = 0.28
 const AIR_ATTACK_RECOVERY_TIME = 0.12
 const INVINCIBILITY_TIME = 0.7
 const ATTACK_MOVE_MULTIPLIER = 0.45
+const ROLL_SPEED = 350.0
+const ROLL_DURATION = 0.6
 
 @onready var anim: AnimatedSprite2D = $FullBodySprite
 @onready var attack_area: Area2D = $AttackArea
@@ -108,10 +110,6 @@ func _physics_process(delta: float) -> void:
 	update_animation()
 	
 	
-func update_sprite_direction() -> void:
-	var should_flip := facing_direction < 0
-	anim.flip_h = should_flip
-
 func update_sprite_direction() -> void:
 	var should_flip := facing_direction < 0
 	anim.flip_h = should_flip
